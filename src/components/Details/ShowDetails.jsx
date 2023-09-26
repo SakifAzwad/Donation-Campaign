e114e49a93663e64746d58f5dd952967e66a1659
SakifAzwad <sakifazwad18@gmail.com>
2023-09-27 04:14:59 +0600

    Stored Data in Local Storage and added sweetalert

--- src/components/Details/ShowDetails.jsx
+++ src/components/Details/ShowDetails.jsx
@@ -1,23 +1,75 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-const ShowDetails = ({data}) => {
-    //console.log(data);
-    const divStyle = {
-        backgroundColor: 'rgba(11, 11, 11, 0.5)', // Red color with 50% opacity
-      };
-    const {id,picture,title,category,category_bg,card_bg,text_col,description,price}=data;
-    return (
-        <div>
-            <img className="w-full h-[500px] mx-auto px-32 mt-8 " src={picture} alt="" />
-            <div style={divStyle} className="relative -top-24 mx-32 h-24 rounded-b-lg">
-                <button style={{backgroundColor:text_col}} className="text-white rounded-lg px-4 py-3 mt-5 ml-8 text-lg font-bold">Donate {price}</button>
-            </div>
-            <h1 className="relative -top-8 font-bold text-4xl mx-32">{title}</h1>
-            <p className="mx-32 mb-24 text-base font-normal">{description}</p>
 
+import swal from 'sweetalert';
 
-        </div>
-    );
+const ShowDetails = ({ data }) => {
+  const divStyle = {
+    backgroundColor: "rgba(11, 11, 11, 0.5)", // Red color with 50% opacity
+  };
+  const {
+    id,
+    picture,
+    title,
+    category,
+    category_bg,
+    card_bg,
+    text_col,
+    description,
+    price,
+  } = data;
+
+  const buttonClick = () => {
+    const addedDataArray = [];
+
+    const Items = JSON.parse(localStorage.getItem("item"));
+
+    //jokhon kisu nai tokhon e if vitor dhukba
+    if (!Items) {
+      addedDataArray.push(data);
+      localStorage.setItem("item", JSON.stringify(addedDataArray));
+      swal("Congratulations", "Thanks for donating!!", "success");
+    } else {
+      const isExits = Items.find((data) => data.id === id);
+
+      if (!isExits) {
+        addedDataArray.push(...Items, data);
+        localStorage.setItem("item", JSON.stringify(addedDataArray));
+        swal("Congratulations", "Thanks for donating!!", "success");
+      } else {
+        swal("Congratulations", "Thanks for donating again!!", "success");
+      }
+    }
+
+    // localStorage.setItem('test',JSON.stringify([{name:"hasib"},{name:"ph"}]))
+  };
+
+  return (
+    <div>
+      <img
+        className="w-full h-[500px] mx-auto px-32 mt-8 "
+        src={picture}
+        alt=""
+      />
+      <div
+        style={divStyle}
+        className="relative -top-24 mx-32 h-24 rounded-b-lg"
+      >
+        <button
+          onClick={() => {
+            buttonClick();
+          }}
+          style={{ backgroundColor: text_col }}
+          className="text-white rounded-lg px-4 py-3 mt-5 ml-8 text-lg font-bold"
+        >
+          Donate {price}
+        </button>
+        
+      </div>
+      <h1 className="relative -top-8 font-bold text-4xl mx-32">{title}</h1>
+      <p className="mx-32 mb-24 text-base font-normal">{description}</p>
+    </div>
+  );
 };
 
 export default ShowDetails;
