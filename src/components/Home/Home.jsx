bde9d984696fb2943b383a57f43f38561d88abb4
SakifAzwad <sakifazwad18@gmail.com>
2023-09-27 19:41:05 +0600

    Added Search Button filter feature

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import Data from "../Data/Data";
 import useGetData from "../hook/useGetData";
 import "./Home.css";
@@ -5,7 +7,21 @@ import "./Home.css";
 const Home = () => {
 
     const [datas] = useGetData()
+    const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
 
+  const handleSearch = () => {
+    setIsSearching(true);
+
+    const filteredResults = datas.filter(
+      (item) =>
+        item.category.toLowerCase() === searchTerm.toLowerCase()
+    );
+
+    setSearchResults(filteredResults);
+    setIsSearching(false);
+  };
 
   return (
     <div>
@@ -15,14 +31,24 @@ const Home = () => {
           I Grow By Helping People In Need
         </h1>
         <div className="sea1 flex justify-center items-center">
-        <input className="lg:w-1/4 px-7 py-3.5 rounded-l-lg border-r-0 border-2 border-solid" type="text" placeholder="Search here...."/>
+        <input className="lg:w-1/4 px-7 py-3.5 rounded-l-lg border-r-0 border-2 border-solid" type="text" value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search here...."/>
         
-        <button className="text-lg text-white rounded-r-lg font-semibold bg-[#FF444A] px-7 py-[13px]">
+        <button onClick={handleSearch} disabled={isSearching} className="text-lg text-white rounded-r-lg font-semibold bg-[#FF444A] px-7 py-[13px]">
               Search
             </button>
         </div>
       </div>
-        <Data datas={datas}></Data>
+        {
+          searchResults.length>0 ? (
+
+            <Data datas={searchResults}></Data>
+          ) : (
+
+            <Data datas={datas}></Data>
+          )
+
+        }
     </div>
   );
 };
